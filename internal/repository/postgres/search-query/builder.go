0e2a570a1d4d24c4a38e57d0977e9b2b4a78e150
Demzez <kirill5738303@gmail.com>
2026-05-04 21:31:50 +0300

    fix: repair bugs with query builder

--- internal/repository/postgres/search-query/builder.go
+++ internal/repository/postgres/search-query/builder.go
@@ -2,11 +2,11 @@ package search_query
 
 func BuildTransactionQuery(like string) (string, string) {
 	base := `SELECT transaction_id, hash, source, amount, direction, status, created_at, updated_at FROM "transaction"`
-
+	
 	if like == "" {
 		return base, ""
 	}
-
+	
 	q := base + `
 		WHERE
 			hash ILIKE $1 OR
@@ -14,34 +14,34 @@ func BuildTransactionQuery(like string) (string, string) {
 			amount::text LIKE $1 OR
 			direction ILIKE $1 OR
 			status ILIKE $1`
-
+	
 	return q, "%" + like + "%"
 }
 
 func BuildDoubtfulTransactionQuery(like string) (string, string) {
 	base := `SELECT transaction_id, risk_score, description, decision FROM "doubtful_transaction"`
-
+	
 	if like == "" {
 		return base, ""
 	}
-
+	
 	q := base + `
 		WHERE
 			transaction_id::text LIKE $1 OR
 			risk_score::text LIKE $1 OR
 			description ILIKE $1 OR
-			decision ILIKE $1 OR`
-
+			decision ILIKE $1`
+	
 	return q, "%" + like + "%"
 }
 
 func BuildFraudRuleQuery(like string) (string, string) {
 	base := `SELECT rule_id, name, active, field_name, operator, value, add_risk FROM "fraud_rule"`
-
+	
 	if like == "" {
 		return base, ""
 	}
-
+	
 	q := base + `
 		WHERE
 			rule_id::text LIKE $1 OR
@@ -50,23 +50,23 @@ func BuildFraudRuleQuery(like string) (string, string) {
 			field_name ILIKE $1 OR
 			operator ILIKE $1 OR
 			value ILIKE $1 OR
-			add_risk::text LIKE $1 OR`
-
+			add_risk::text LIKE $1`
+	
 	return q, "%" + like + "%"
 }
 
 func BuildUserQuery(like string) (string, string) {
-	base := `SELECT user_id, login, password, role_id, created_at FROM "user"`
-
+	base := `SELECT user_id, login, role_id, created_at FROM "user"`
+	
 	if like == "" {
 		return base, ""
 	}
-
+	
 	q := base + `
 		WHERE
 			user_id::text LIKE $1 OR
 			login ILIKE $1 OR
-			role_id::test LIKE $1 OR`
-
+			role_id::text LIKE $1`
+	
 	return q, "%" + like + "%"
 }
